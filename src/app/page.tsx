2c04c6f1b8772b84ebcbe9dc490be614e34ac287
yuki <yuki.for.neutral@gmail.com>
2024-08-16 16:03:21 +0900

    add flex and user setting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
   };
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="flex flex-col justify-center items-center container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Todo App</h1>
       <div className="mb-4">
         <input
